2bbfec5e504f5d49be86a0d1d9bbf2cdbc6481d8
Eric Djatsa Yota <ericdjatsa@google.com>
2025-05-19 16:31:04 +0000

    Added more features: disabled, incremental, partitioning, ...

--- includes/constants.js
+++ includes/constants.js
@@ -1,2 +1,4 @@
 const RAW_DATA_ROOT_PATH = "gs://myericanalytics-temp/the_look_ecommerce";
-module.exports = { RAW_DATA_ROOT_PATH };
+const DEV_PARTITION_EXPIRATION_DAYS = 90 ;
+const init = "false";
+module.exports = { RAW_DATA_ROOT_PATH , DEV_PARTITION_EXPIRATION_DAYS, init};
